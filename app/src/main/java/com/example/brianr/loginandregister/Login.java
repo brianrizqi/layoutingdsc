0473d58045a9ec762e7db4f309c8fd7f0603db77
Brian Rizqi <30972350+brianrizqi@users.noreply.github.com>
2018-07-27 19:29:37 +0700

    initial commit

---
+++ app/src/main/java/com/example/brianr/loginandregister/Login.java
@@ -0,0 +1,47 @@
+package com.example.brianr.loginandregister;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Login extends AppCompatActivity {
+    private EditText username, password;
+    private Button login, regis;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username = (EditText) findViewById(R.id.userLogin);
+        password = (EditText) findViewById(R.id.passLogin);
+        regis = (Button) findViewById(R.id.buttonRegis);
+        login = (Button) findViewById(R.id.btnLogin);
+        regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, Register.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/com/example/brianr/loginandregister/Register.java
@@ -0,0 +1,47 @@
+package com.example.brianr.loginandregister;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Register extends AppCompatActivity {
+    private EditText email, username, password;
+    private Button regis;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        email = (EditText) findViewById(R.id.emailRegis);
+        username = (EditText) findViewById(R.id.userRegis);
+        password = (EditText) findViewById(R.id.passRegis);
+        regis = (Button) findViewById(R.id.btnRegis);
+        regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Register.this, Login.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
