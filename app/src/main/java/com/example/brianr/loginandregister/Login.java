dd1313320add2a7df2bb4d2e8622562e54292346
myfcibaicode <34906923+myfcibaicode@users.noreply.github.com>
2018-07-28 09:18:40 +0700

    BackEnd

--- app/src/main/java/com/example/brianr/loginandregister/Login.java
+++ app/src/main/java/com/example/brianr/loginandregister/Login.java
@@ -3,6 +3,7 @@ package com.example.brianr.loginandregister;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,29 +20,64 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Login extends AppCompatActivity {
-    private EditText username, password;
-    private Button login, regis;
+    private EditText usernameEditText, passwordEditText;
+    private Button loginBtn, regisBtn;
+    private ArrayList<User> users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        username = (EditText) findViewById(R.id.userLogin);
-        password = (EditText) findViewById(R.id.passLogin);
-        regis = (Button) findViewById(R.id.buttonRegis);
-        login = (Button) findViewById(R.id.btnLogin);
-        regis.setOnClickListener(new View.OnClickListener() {
+        // Init
+        users = new ArrayList<User>();
+        usernameEditText = (EditText) findViewById(R.id.userLogin);
+        passwordEditText = (EditText) findViewById(R.id.passLogin);
+        regisBtn = (Button) findViewById(R.id.buttonRegis);
+        loginBtn = (Button) findViewById(R.id.btnLogin);
+        // CLick Listener
+        regisBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent i = new Intent(Login.this, Register.class);
-                startActivity(i);
-                finish();
+                startActivityForResult(i, 999);
             }
         });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                boolean status = false;
+                for (int i=0; i<users.size(); i++){
+                    User user = users.get(i);
+                    if(user.getUsername().equals(username) && user.getPassword().equals(password)){
+                        // Masuk ke Dashboard
+                        Toast.makeText(Login.this, "Halo Bro", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                // Cek jika tidak ditemukan
+                if(status == false){
+                    Toast.makeText(Login.this, "Data tidak cocok", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 999 && resultCode == RESULT_OK){
+            User user = (User) data.getSerializableExtra("new_user");
+            users.add(user);
+            // Opsional clear EditText
+            usernameEditText.setText("");
+            passwordEditText.setText("");
+        }
     }
 }
 
